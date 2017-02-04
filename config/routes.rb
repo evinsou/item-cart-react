c6ea4cc92ce8d6a9cd9bd27d6a99895e155558e6
Ivan <evinsou@gmail.com>
2017-02-04 17:16:03 +0200

    Add Site controller and files for it

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'site#index'
-
   namespace :api do
     namespace :v1 do
       resources :items, only: [:index, :create, :destroy, :update]
     end
   end
+
+  root to: 'site#index'
 end
